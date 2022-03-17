05a4b39c43fbbb58811088c72c9b8afb8a38764f
mayanktamrkar <mayanktamrkar786@gmail.com>
2022-03-17 09:55:50 +0530

    firtst

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+const app = express()
+const port = process.env.PORT || '3000'
+import {join} from 'path'
+import about from './routes/home.js'
+import {connectDB}  from './db/connect.js'
+
+app.use(express.urlencoded({extended:false}))
+app.use('/static',express.static(join(process.cwd(),'public')))
+app.use('/',about)
+const DATABASE_URL = process.env.DATABASE_URL||"mongodb://localhost:27017"
+app.set('view engine','ejs')
+
+//connect database 
+//connect database 
+connectDB(DATABASE_URL)
+
+
+app.listen(port,(req,res)=>{
+    console.log(`server run on http://localhost:${port}`)
+})
