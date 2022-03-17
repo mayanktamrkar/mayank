40e3f0412295b60128a1bc11ce70103e346999a7
mayanktamrkar <62329309+mayanktamrkar@users.noreply.github.com>
2022-03-17 10:14:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const app = express()
-const port = process.env.PORT || '3000'
+const port = process.env.PORT || 'mayank-ap12.herokuapp.com'
 import {join} from 'path'
 import about from './routes/home.js'
 import {connectDB}  from './db/connect.js'
